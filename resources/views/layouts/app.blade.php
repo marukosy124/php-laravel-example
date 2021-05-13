240cf552c29987bcebc3a6427477f0e1fd3a57cb
marukosy124 <soniayeung12435@gmail.com>
2021-05-13 16:19:30 +0800

    add controller and register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,37 @@
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
-<body>
+<body class="bg-gray-200">
+  <nav class="p-6 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+      <li>
+        <a href="/" class="p-3">Home</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Dashboard</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Posts</a>
+      </li>
+    </ul>
+
+    <ul class="flex items-center">
+      <li>
+        <a href="/" class="p-3">Damn</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Login</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Register</a>
+      </li>
+      <li>
+        <a href="" class="p-3">Logout</a>
+      </li>
+    </ul>
+
+  </nav>
 </body>
-  @yield('content')
+@yield('content')
+
 </html>
